0aa101b65d1bf2156e8215444ee38944bf19e9c1
dradientgescent <33252453+dradientgescent@users.noreply.github.com>
2019-07-22 18:50:08 +0530

    Update train.py

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from keras.backend.tensorflow_backend import set_session
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
-config.gpu_options.visible_device_list = "0"
+config.gpu_options.visible_device_list = "2"
 set_session(tf.Session(config=config))
 
 
